1547e4deb0b2e15a8b294d3dd97506955b4b784c
DigtUser <80969886+DigtUser@users.noreply.github.com>
2021-09-20 22:01:38 +0300

    Update config.js

--- config.js
+++ config.js
@@ -37,7 +37,7 @@ const config = {
   },
   sidebar: {
     forcedNavOrder: ['v1.2.026','v1.2.042','v1.3'],
-    collapsedNav: ["/v1.2.026","/v1.2.042","/v1.3"],
+    collapsedNav: [],
     links: [{ text: 'Trusted.Plus', link: 'https://trusted.plus' }],
     frontline: false,
     ignoreIndex: true,
